55853eb0406509cbe95970bfb14bf95d7ae289a8
Thomas Moore <tom4pres@gmail.com>
2018-02-11 14:53:21 -0600

    rearranged structure

---
+++ install.py
@@ -0,0 +1,12 @@
+import os
+from maya import cmds
+
+# find path to this file
+
+# find maya module path
+# make sure it is cross platform
+# make modules folder if doesn't exist
+
+# uninstall
+# copy
+
