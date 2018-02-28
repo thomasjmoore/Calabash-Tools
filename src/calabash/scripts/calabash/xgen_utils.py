fdf96561d0b724f89205e5a84345a11e8ccfbd26
Thomas Moore <tom4pres@gmail.com>
2018-02-27 22:55:06 -0600

    repath toggle support

--- src/calabash/scripts/calabash/xgen_utils.py
+++ src/calabash/scripts/calabash/xgen_utils.py
@@ -6,8 +6,8 @@ import os
 import maya.OpenMayaAnim as oma
 
 
-def cache_groomableSplines(desc = []):
-    mel.eval('ogs -pause;')
+def cache_groomableSplines(desc=[]):
+
     start = int(oma.MAnimControl.minTime().value())
     end = int(oma.MAnimControl.maxTime().value())
     relative_sample = 0
@@ -19,25 +19,27 @@ def cache_groomableSplines(desc = []):
     fileName, num, ext = file.split(".")
 
     curProj = cmds.workspace(fn=True, q=True)
-    print fileName
-    if not os.path.exists(os.path.join(curProj, "cache", "alembic", fileName)):
-        os.mkdir(os.path.join(curProj, "cache", "alembic", fileName))
+    alembic_path = os.path.join(curProj, "cache", "alembic")
+    cache_dir = os.path.join(alembic_path, fileName)
 
+    if not os.path.exists(alembic_path):
+        os.mkdir(alembic_path)
+
+    if not os.path.exists(cache_dir):
+        os.mkdir(cache_dir)
 
     sel = cmds.ls(sl=True)
     if not desc and sel:
-        #shapes = getShapes()
-
         for s in sel:
             if cmds.xgmSplineQuery(s, isSplineDescription=True):
                 desc.append(s)
 
-
     if not desc:
-        print ("No XGen Descriptions Selected, Caching all in scene")
+        cmds.warning("No XGen Descriptions Selected, Caching all in scene...")
         desc = cmds.xgmSplineQuery(listSplineDescriptions=True)
 
-    #print desc
+    if not cmds.ogs(q=True, pause=True):
+        cmds.ogs(pause=True)
 
     for d in desc:
         cmds.select(d)
@@ -46,46 +48,68 @@ def cache_groomableSplines(desc = []):
         cache_name = "%s_%s.abc" % (fileName, d_removeNS)
 
         cache_path = "/".join([curProj, "cache", "alembic", fileName, cache_name])
-        #print cache_path
 
-        #mel.eval('xgmSplineCache -create -j -file "Z:/raid/3Dprojects/maya/projects/Pikmi_Pops/data/testCache52.abc" -df ogawa -fr 1 62 -step 1 -obj ANIM:EBB:ebby_feet;')
-        #mel.eval('tmXgmSplineCacheExportCmd -create "Z:/raid/3Dprojects/maya/projects/Pikmi_Pops/data/testCache02.abc" 1  10 {"pichiTail"}')
         melCmd = ('tmXgmSplineCacheExportCmd -create "%s" %s %s %s %s %s' % (cache_path, start, end, relative_sample, sample_low, sample_high))
         #print melCmd
 
         mel.eval(melCmd)
 
-    mel.eval('ogs -pause;')
-    #repath_caches()
+    cmds.ogs(pause=True)
+
 
 def repath_caches():
+    repath = cmds.confirmDialog(title="Repath Caches",
+                                   message="Repath Caches",
+                                   button=["Local", "Renderfarm",    "Cancel"],
+                                   defaultButton="Cancel",
+                                   cancelButton="Cancel",
+                                   dismissString="Cancel")
+    if repath == "Cancel":
+        cmds.warning("Action canceled")
+        return
+
+
     sel = cmds.ls(sl=True, type="xgmSplineCache")
     if not sel:
         caches = cmds.ls(type="xgmSplineCache")
     else:
         caches = sel
 
-    for cache in caches:
-        loaded_data = cmds.getAttr("%s.activeDescription"%cache)
-        print loaded_data
+    if repath == "Renderfarm":
+        for cache in caches:
+            loaded_data = cmds.getAttr("%s.activeDescription"%cache)
+            print loaded_data
 
-        curProj = os.path.basename(cmds.workspace(sn=True, q=True))
-        val = cmds.getAttr("%s.fileName"%cache)
-        valSplit = splitpath(val)
-        del valSplit[0:-4]
-        print valSplit
-        network_path = r"\raid\3Dprojects\maya\projects"
-        combined_path = [network_path] + [curProj] + valSplit
-        print combined_path
-        new_path = "\\".join(combined_path)
-        #new_path = "FRANCIS\\Users\\tom4p\\Desktop\%s"% valSplit[-1]
-        #print new_path
+            curProj = os.path.basename(cmds.workspace(sn=True, q=True))
+            val = cmds.getAttr("%s.fileName"%cache)
+            valSplit = splitpath(val)
+            del valSplit[0:-4]
+            print valSplit
+            network_path = r"\raid\3Dprojects\maya\projects"
+            combined_path = [network_path] + [curProj] + valSplit
+            new_path = "\\".join(combined_path)
+            #print new_path
 
-        cmds.setAttr("%s.fileName"%cache, new_path, type="string")
-        #cmds.setAttr("%s.fileName"%cache, "\\\\FRANCIS\\Users\\tom4p\\Desktop\%s"% valSplit[-1], type="string")
-        #cmds.setAttr("%s.fileName"%cache, val, type="string")
-        cmds.setAttr("%s.activeDescription"%cache, loaded_data, type="string")
-        print("Repathed %s to %s"%(cache, new_path))
+            cmds.setAttr("%s.fileName"%cache, new_path, type="string")
+            cmds.setAttr("%s.activeDescription"%cache, loaded_data, type="string")
+            print("Repathed %s to %s"%(cache, new_path))
+
+    if repath == "Local":
+        for cache in caches:
+            loaded_data = cmds.getAttr("%s.activeDescription" % cache)
+
+            curProj = cmds.workspace(fn=True, q=True)
+            val = cmds.getAttr("%s.fileName"%cache)
+            valSplit = splitpath(val)
+            del valSplit[0:-4]
+
+            combined_path = [curProj] + valSplit
+            new_path = os.path.join(*combined_path)
+
+            cmds.setAttr("%s.fileName"%cache, new_path, type="string")
+            cmds.setAttr("%s.activeDescription" % cache, loaded_data, type="string")
+            cmds.warning("Repathed %s to %s" % (cache, new_path))
+
 
 def getShapes():
     sel = cmds.ls(sl=1, l=1)
